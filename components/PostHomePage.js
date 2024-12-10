78009f7061cb9650a688a82ca65938f19f3442f0
Shady Idrissi <chadiidac@gmail.com>
2024-12-10 22:38:59 +0100

    add some editing

--- components/PostHomePage.js
+++ components/PostHomePage.js
@@ -7,11 +7,16 @@ function PostHomePage() {
             <div className='card'>
                 <ul className='header-card'>
                     <li>
-                        <img src="" alt="" />
+                        <img src="https://avatars.githubusercontent.com/u/137694413?v=4" alt="" />
                         <p>qqqqq</p>
                     </li>
                     <li>
-                        <p id='time'>12:00:00</p>
+                        <p id='time'>
+                            12:00:00
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                            </svg> 
+                        </p>
                     </li>
                 </ul>
                 <ul className='text-card'>
@@ -33,7 +38,7 @@ function PostHomePage() {
                         : Hell
                     </Link>
                     <Link href=''>
-                    See More
+                        See More
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="3.5" stroke="currentColor" className="size-3">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
                         </svg>
